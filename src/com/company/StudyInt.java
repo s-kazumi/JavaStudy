bd59f37658fccd399a675d092a1ff0f041481758
s-kazumi <77275553+s-kazumi@users.noreply.github.com>
2021-01-11 21:22:41 +0900

    overfrowing digits

---
+++ src/com/company/StudyInt.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class StudyInt {
+
+    public static void main(String args[]) {
+        int i = Integer.MAX_VALUE;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+    }
+}
